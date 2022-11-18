effc836737e53c8307f960a7c289ca3b8a6c845b
supat-roong <supatpooh123@gmail.com>
2022-11-18 14:31:48 +0900

    add comment in data viz function

--- data_visualization.py
+++ data_visualization.py
@@ -1,37 +1,40 @@
 import os
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 
-PLOT_TITLE = "Time-Series Prediction"
 
-
-def plot_input_data(column_name_list, input_data, output_image_name):
+def plot_input_data(column_name_list, input_data, plot_title, output_image_name):
     """
     Visualize input data to time-series graph by column
 
     Args:
         column_name_list (np.array)
         input_data (np.array): array of input data corresponding to column_name_list
+        plot_title (str)
         output_image_name (str)
     """
     num_plot, num_col, num_row = get_plot_dimension(column_name_list)
     # Initiate subplot
     fig, axs = plt.subplots(num_row, num_col)
-    plt.suptitle(PLOT_TITLE)
+    plt.suptitle(plot_title)
     column_name_matrix = np.resize(column_name_list, (num_row, num_col))
 
+    # Plot subplot for each column
     for i in range(num_row):
         for j in range(num_col):
             count_num_plot = num_col * i + j
             axs[i, j].plot(input_data[:, count_num_plot])
             axs[i, j].set_title(column_name_matrix[i, j])
 
+            # Check if finish plotting
             if (count_num_plot + 1) == num_plot:
+                # Remove inner axis label
                 for ax in axs.flat:
                     ax.label_outer()
 
+                # Save graph
                 fig.savefig(f"{output_image_name}.pdf", bbox_inches="tight")
+                # Convert to png
                 os.system(
                     f"pdftoppm -png -r 300 -singlefile {output_image_name}.pdf {output_image_name}"
                 )
@@ -43,6 +46,7 @@ def plot_prediction(
     input_data,
     prediction_data,
     train_size,
+    plot_title,
     output_image_name,
     do_compare_result,
 ):
@@ -54,16 +58,19 @@ def plot_prediction(
         input_data (np.array): array of input data corresponding to column_name_list
         prediction_data (np.array): array of prediction data
         train_size (int): training size
+        plot_title (str)
         output_image_name (str)
         do_compare_result (bool): flag to also plot input data for comparison
     """
+    # Check compare result flag
     if do_compare_result:
         num_plot, num_col, num_row = get_plot_dimension(column_name_list)
         # Initiate subplot
         fig, axs = plt.subplots(num_row, num_col)
-        plt.suptitle(PLOT_TITLE)
+        plt.suptitle(plot_title)
         column_name_matrix = np.resize(column_name_list, (num_row, num_col))
 
+        # Plot subplot for each column
         for i in range(num_row):
             for j in range(num_col):
                 count_num_plot = num_col * i + j
@@ -72,10 +79,13 @@ def plot_prediction(
                 axs[i, j].set_title(column_name_matrix[i, j])
                 axs[i, j].axvline(x=train_size, c="r", linestyle="--")
 
+                # Check if finish plotting
                 if (count_num_plot + 1) == num_plot:
+                    # Remove inner axis label
                     for ax in axs.flat:
                         ax.label_outer()
 
+                    # Add legend
                     plt.legend(
                         ["Ground Truth", "Prediction"],
                         loc="lower center",
@@ -83,19 +93,23 @@ def plot_prediction(
                         bbox_transform=plt.gcf().transFigure,
                         ncol=2,
                     )
-
+                    # Save graph
                     fig.savefig(f"{output_image_name}.pdf", bbox_inches="tight")
+                    # Convert to png
                     os.system(
                         f"pdftoppm -png -r 300 -singlefile {output_image_name}.pdf {output_image_name}"
                     )
                     return
 
     else:
-        plot_input_data(column_name_list, prediction_data, output_image_name)
+        plot_input_data(
+            column_name_list, prediction_data, plot_title, output_image_name
+        )
 
 
 def get_plot_dimension(column_name_list):
-    """Find subplot dimension
+    """
+    Find subplot dimension
 
     Args:
         column_name_list (np.array)
@@ -107,15 +121,3 @@ def get_plot_dimension(column_name_list):
     num_col = int(np.ceil(np.sqrt(num_plot)))
     num_row = int(np.ceil(num_plot / num_col))
     return num_plot, num_col, num_row
-
-
-# pwd = os.getcwd()
-# data_folder = "assets/data"
-# file_name = "test3_2_waypoints.csv"
-# df = pd.read_csv(os.path.join(pwd, data_folder, file_name))
-# df = df[["x", "y", "z", "yaw"]]
-# input_data = df.values
-# column_name_list = df.columns.to_numpy()
-# output_image_name = "predict"
-# plot_prediction(column_name_list, input_data, input_data, 600, output_image_name, False)
-
